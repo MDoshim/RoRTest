4bb895b6beecc1d5d985f67a6eff5abf9a8eef31
Autuno rbenv <michael@ubuntu.(none)>
2013-11-12 19:40:38 -0800

    Hello

---
+++ ryan/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ryan::Application.config.secret_key_base = '1acf552579db7887c2a43dee9d6254a5983bdc520142c9ff401277e46e94d5cb67d030af9d3687eb8308c098d123d108db61173084eceaf7bb31f010fa7bd71b'
